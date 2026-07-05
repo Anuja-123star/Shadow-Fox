593f2b267b443d11442fc5b131accc4ed54c1399
Anuja SK <anujask313@gmail.com>
2026-07-05 22:41:19 +0530

    Add main.py to calculate simple interest and pi

---
+++ Task -1/main.py
@@ -0,0 +1,21 @@
+# Task 1 - Variables
+
+pi = 22 / 7
+
+print("Value of pi:", pi)
+print("Data type of pi:", type(pi))
+
+print("\nAttempting to use 'for' as a variable name:")
+print("SyntaxError: 'for' is a reserved keyword in Python and cannot be used as a variable name.")
+
+principal = 10000
+rate = 5
+time = 3
+
+simple_interest = (principal * rate * time) / 100
+
+print("\nSimple Interest")
+print("Principal:", principal)
+print("Rate:", rate, "%")
+print("Time:", time, "years")
+print("Simple Interest:", simple_interest)
