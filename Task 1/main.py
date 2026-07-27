8a6ef1b01b792ed3979da450bbfad8c1fb0751b0
Anuja SK <anujask313@gmail.com>
2026-07-27 19:56:26 +0530

    Add main.py with tasks for pi and simple interest

---
+++ Task 1/main.py
@@ -0,0 +1,23 @@
+# Task 1
+pi = 22 / 7
+print("Task 1")
+print("Value of pi:", pi)
+print("Data type:", type(pi))
+
+print("\nTask 2")
+print("The variable name 'for' cannot be used because it is a reserved keyword in Python.")
+
+# If you try this, it will give an error:
+# for = 4
+
+print("\nTask 3")
+p = 10000
+r = 5
+t = 3
+
+si = (p * r * t) / 100
+
+print("Principal:", p)
+print("Rate:", r)
+print("Time:", t)
+print("Simple Interest:", si)
